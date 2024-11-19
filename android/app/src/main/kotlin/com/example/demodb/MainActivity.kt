0fcd5d1a89f6e3dd1ac6b3f9f3dfef5a432f3bce
Siddh <rj07605@gmail.com>
2024-11-19 13:55:24 +0530

    Initial commit - Setting up the project

---
+++ android/app/src/main/kotlin/com/example/demodb/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.demodb
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Column(
+        children: [
+          // logo
+
+          // welcome back message
+
+          // pw textfield
+
+          // register
+        ],
+      ),
+    );
+  }
+}
