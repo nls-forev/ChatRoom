0595dfe1d7ab7e717181db5e6bf761f7eaa1ffbf
Siddh <rj07605@gmail.com>
2024-11-19 17:17:35 +0530

    App successfully connected to Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:demodb/auth/login_or_register.dart';
+import 'package:demodb/firebase_options.dart';
 import 'package:demodb/themes/light_mode.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
